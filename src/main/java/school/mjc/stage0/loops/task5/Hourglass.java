e6fc17ad573d8c08dea6e60fce48a7253e7ae17b
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-11 19:10:46 +0300

    first commit

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        for(int i=0; i<height; i++){
+            if(i<(height/2)) {
+                for (int j = 0; j < height; j ++) {
+                    if(j>=i && j<=height-i-1){
+                        System.out.print(8);
+                    }else{
+                        System.out.print(" ");
+                    }
+                }
+            }else{
+                for (int j =0; j <height; j++) {
+                    if(j<=i && j>=height-i-1){
+                        System.out.print(8);
+                    }else{
+                        System.out.print(" ");
+                    }
 
+                }
+            }
+            System.out.println();
+        }
     }
 }
